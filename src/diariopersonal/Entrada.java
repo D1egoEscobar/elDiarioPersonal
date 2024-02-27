a57453597db573c6aa14f6f49f423024103c8218
Diego Escobar <diegofe.escobar@umariana.edu.co>
2024-02-26 23:47:22 -0500

    Version1

---
+++ src/diariopersonal/Entrada.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package diariopersonal;
+
+/**
+ *
+ * @author Diego Escobar
+ */
+public class Entrada {
+    private int idEntrada;
+    private String fecha;
+    private String descripcion;
+
+    public Entrada(int idEntrada, String fecha, String descripcion) {
+        this.idEntrada = idEntrada;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+    }
+
+
+    public int getIdEntrada() {
+        return idEntrada;
+    }
+
+    public void setIdEntrada(int idEntrada) {
+        this.idEntrada = idEntrada;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
+
